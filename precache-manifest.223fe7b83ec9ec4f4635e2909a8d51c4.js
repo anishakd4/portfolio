49219e8d5e26aad62ca158038b7cca4c950fcfb0
anish <anish.dubey@citymall.live>
2022-02-11 20:56:25 +0530

    Updates

---
+++ precache-manifest.223fe7b83ec9ec4f4635e2909a8d51c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7375a9ddb3ad0e801b8",
+    "url": "/me/static/css/main.ebafef61.chunk.css"
+  },
+  {
+    "revision": "f7375a9ddb3ad0e801b8",
+    "url": "/me/static/js/main.f7375a9d.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "45edbbbd38934650cd3c4200de9be78a",
+    "url": "/me/index.html"
+  }
+];
